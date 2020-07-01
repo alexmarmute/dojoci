ff81746eff30c4bff7aaae4f8cce66c2bad13297
marmute14@gmail.com <marmute14@gmail.com>
2020-06-30 22:38:21 -0300

    testes do sistema vendas

---
+++ sistema-vendas.testes/CalculadoraComisao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace com.br.k21.SistemaVendas.Testes
+{
+    public class CalculadoraComisao
+    {
+        const int choosenSaleValue = 10000;
+        const double taxLow = 0.05;
+        const double taxHigh = 0.06;
+
+        public double CalculateComission(int saleValue)
+        {
+            if (saleValue <= choosenSaleValue)
+            {
+                return saleValue * taxLow;
+            }
+            else
+            {
+                return saleValue * taxHigh;
+            }
+        }
+    }
+}
---
+++ sistema-vendas.testes/TesteCalculadoraComissao.cs
@@ -0,0 +1,46 @@
+﻿//Passo 1: Escrever um teste para obter um "vermelho"
+//Passo 2: Escrever uma solução mínima para obter um "verde"
+//Passo 3: Melhorar código para continuar no "verde" e retornar ao passo 1
+
+//Critérios de Aceitação:
+//Se a venda for menor ou igual a 10000, comissão de 5%
+//Se a venda for maior que 10000, comissão de 6%
+
+//Tiago Fernandos
+// Ruben
+// Margarida
+// Luis
+// Ana
+// Pedro
+// Ruben
+
+
+using NUnit.Framework;
+
+namespace com.br.k21.SistemaVendas.Testes
+{
+    public class TestesCalculadoraComissao
+    {
+        [Test]
+        public void umaVendaDe8000RetornaComissaoDe400()
+        {
+            int valorVenda = 8000;
+            double valorComissao = 400;
+
+            double comissaoEsperado = new CalculadoraComisao().CalculateComission(valorVenda);
+
+            Assert.AreEqual(valorComissao, comissaoEsperado);
+        }
+
+        [Test]
+        public void umaVendaDe12000RetornaComissaoDe720()
+        {
+            int valorVenda = 12000;
+            double valorComissao = 720;
+
+            double comissaoEsperado = new CalculadoraComisao().CalculateComission(valorVenda);
+
+            Assert.AreEqual(valorComissao, comissaoEsperado);
+        }
+    }
+}
